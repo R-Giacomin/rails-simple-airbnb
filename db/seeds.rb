fd82ad5faeaab8f4bba8991c2b242d503329ab8a
Rafael Giacomin <faellg@gmail.com>
2020-09-17 23:00:48 -0300

    Submeto exercício simple_airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Casa grande da família porcos',
+  address: 'Rua 1 Porco',
+  picture_url: 'https://source.unsplash.com/1600x900/?house',
+  description: 'Casa bem grande e espaçosa',
+  price_per_night: 200,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Casa de palha',
+  address: 'Rua 3 Porcos',
+  picture_url: 'https://source.unsplash.com/1600x900/?straw_house',
+  description: 'Casa bem simples de palha',
+  price_per_night: 10,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Casa de madeira',
+  address: 'Rua 4 Porcos',
+  picture_url: 'https://source.unsplash.com/1600x900/?wood_house',
+  description: 'Casa bem simples de madeira',
+  price_per_night: 35,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Casa de tijolos',
+  address: 'Rua 5 Porcos',
+  picture_url: 'https://source.unsplash.com/1600x900/?brick_house',
+  description: 'Casa bem estruturada de tijolos',
+  price_per_night: 100,
+  number_of_guests: 3
+)
